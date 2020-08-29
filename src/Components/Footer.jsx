ce00e43970841472da668d394a52975705f5ce25
jvnaveenbabu <naveenbabulucky264@gmail.com>
2020-08-30 01:57:16 +0530

    Added src

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <img
+        src="https://img.shields.io/badge/Made%20With-ReactJS-blue?style=for-the-badge&logo=react"
+        alt="javascript-badge"
+        style={{ marginTop: "1rem" }}
+      />
+      <a
+        href="https://github.com/jvnaveenbabu"
+        target="_blank"
+        style={{ marginTop: "5px" }}
+      >
+        <img
+          src="https://img.shields.io/badge/On%20Github-jvnaveenbabu-orange?style=for-the-badge&logo=github"
+          alt="github-badge"
+        />
+      </a>
+      <p>
+        Copyrights &copy; 2020. Made by <span>💙</span>
+      </p>
+    </>
+  );
+};
+
+export default Footer;
