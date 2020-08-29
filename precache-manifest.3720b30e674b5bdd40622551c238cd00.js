d8a3655e2a27a83960af35d876a4a6f4119fb2ec
jvnaveenbabu <naveenbabulucky264@gmail.com>
2020-08-30 02:07:35 +0530

    Updates

---
+++ precache-manifest.3720b30e674b5bdd40622551c238cd00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d59bb4762821870744ff0aecf12684",
+    "url": "/Text-Converter/index.html"
+  },
+  {
+    "revision": "9ddc9238114ca5baa6e6",
+    "url": "/Text-Converter/static/css/main.8dcb2907.chunk.css"
+  },
+  {
+    "revision": "ba95139d7610cefc6594",
+    "url": "/Text-Converter/static/js/2.1c945e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Text-Converter/static/js/2.1c945e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddc9238114ca5baa6e6",
+    "url": "/Text-Converter/static/js/main.2f3f6624.chunk.js"
+  },
+  {
+    "revision": "eb35cadd5903c6273338",
+    "url": "/Text-Converter/static/js/runtime-main.0acb0e4f.js"
+  }
+]);
